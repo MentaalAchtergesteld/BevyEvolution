eebae8dbac7f955f8c89e790ba57b162b40aeced
MentaalAchtergesteld <mentaalachtergesteld@mentaalachtergesteld.nl>
2025-02-07 11:32:51 +0100

    Setup Bevy project with GameRng resource.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use bevy::{app::App, ecs::system::Resource, DefaultPlugins};
+use rand::{rngs::StdRng, SeedableRng};
+
+#[derive(Resource)]
+struct GameRng(StdRng);
+
+impl GameRng {
+    fn new(seed: u64) -> Self {
+        Self(StdRng::seed_from_u64(seed))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(GameRng::new(420))
+        .add_plugins(DefaultPlugins)
+        .run();
 }
